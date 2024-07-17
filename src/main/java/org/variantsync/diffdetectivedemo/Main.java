3a38df8447ba73d2137288e2e86afa5872e84f66
pmbittner <paul.bittner@uni-ulm.de>
2024-07-17 12:54:03 +0200

    fse24 demo variant as on slides

--- src/main/java/org/variantsync/diffdetectivedemo/Main.java
+++ src/main/java/org/variantsync/diffdetectivedemo/Main.java
@@ -5,6 +5,7 @@ import org.variantsync.diffdetective.diff.result.DiffParseException;
 import org.variantsync.diffdetective.show.Show;
 import org.variantsync.diffdetective.show.engine.GameEngine;
 import org.variantsync.diffdetective.variation.DiffLinesLabel;
+import org.variantsync.diffdetective.variation.Label;
 import org.variantsync.diffdetective.variation.diff.VariationDiff;
 import org.variantsync.diffdetective.variation.diff.parse.VariationDiffParseOptions;
 import org.variantsync.diffdetective.variation.tree.VariationTree;
@@ -15,33 +16,38 @@ import java.nio.file.Path;
 
 /**
  * DiffDetective Demo for FSE 2024.
- *
  */
 public class Main
 {
+
+    public static void showDiff(
+            Path fileBefore,
+            Path fileAfter)
+            throws IOException, DiffParseException
+    {
+        VariationDiff<DiffLinesLabel> d = VariationDiff.fromFiles(
+                fileBefore,
+                fileAfter,
+                DiffAlgorithm.SupportedAlgorithm.MYERS,
+                VariationDiffParseOptions.Default);
+        Show.diff(d, "Diff via Git Diff").showAndAwait();
+    }
+
+
+    public static <L extends Label> void showDiff(
+            VariationTree<L> before,
+            VariationTree<L> after)
+    {
+        VariationDiff<L> diffViaGumTree = VariationDiff.fromTrees(before, after);
+        Show.diff(diffViaGumTree, "Diff via GumTree").showAndAwait();
+    }
+
+
     public static void main(String[] args) throws IOException, DiffParseException {
-        Path examplesDir = Path.of("data", "examples");
-        Path pathBefore  = examplesDir.resolve("simple_v1.txt");
-        Path pathAfter   = examplesDir.resolve("simple_v2.txt");
+        Path pathBefore  = Path.of("data", "examples", "simple_v1.txt");
+        Path pathAfter   = Path.of("data", "examples", "simple_v2.txt");
 
-        VariationTree<DiffLinesLabel> s1 = VariationTree.fromFile(pathBefore);
-        VariationTree<DiffLinesLabel> s2 = VariationTree.fromFile(pathAfter);
-
-        VariationDiff<DiffLinesLabel> diffViaGumTree
-                = VariationDiff.fromTrees(s1, s2);
-        VariationDiff<DiffLinesLabel> diffViaGit
-                = VariationDiff.fromFiles(pathBefore, pathAfter,
-                                          DiffAlgorithm.SupportedAlgorithm.MYERS,
-                                          VariationDiffParseOptions.Default);
-        VariationDiff<DiffLinesLabel> diffViaTrueDiff
-                = TrueDiffDetective.compare(s1, s2);
-
-        GameEngine.showAndAwaitAll(
-                Show.tree(s1, "before"),
-                Show.tree(s2, "after"),
-                Show.diff(diffViaGit, "diffViaGit"),
-                Show.diff(diffViaTrueDiff, "diffViaTrueDiff"),
-                Show.diff(diffViaGumTree, "diffViaGumTree")
-        );
+        showDiff(pathBefore, pathAfter);
+        showDiff(VariationTree.fromFile(pathBefore),VariationTree.fromFile(pathAfter));
     }
 }
